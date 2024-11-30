367675db53795fb737ce8cc47101e8c4bc6241ec
Steve Dyson <steven.dyson@pm.me>
2024-11-30 16:21:00 -0500

    feat: finished 2022 day 2 part 2 and cleaned up up entire logic

--- 2022/day2/main.go
+++ 2022/day2/main.go
@@ -6,72 +6,114 @@ import (
 	"os"
 )
 
+type Hand struct {
+	OppChoice HandName
+	Target    HandName
+}
+
+type GameMap struct {
+	Score      int32
+	Defeats    HandName
+	DefeatedBy HandName
+}
+
+type HandName string
+
+const (
+	Rock     HandName = "rock"
+	Paper    HandName = "paper"
+	Scissors HandName = "scissors"
+)
+
+const (
+	WinPoints  = 6
+	DrawPoints = 3
+	LosePoints = 0
+)
+
+var gameMap = map[HandName]GameMap{
+	"rock":     {Score: 1, Defeats: Scissors, DefeatedBy: Paper},
+	"paper":    {Score: 2, Defeats: Rock, DefeatedBy: Scissors},
+	"scissors": {Score: 3, Defeats: Paper, DefeatedBy: Rock},
+}
+
 func main() {
+	var hands []Hand
+
+	// Read the file input
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Println("Error reading file")
 	}
 	defer file.Close()
 
-	var total int32
-
+	// Scan each line in file
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		guide := scanner.Text()
-		choice := string(guide[0])
-		counter := string(guide[2])
+		oppChoice := HandName(guide[0])
+		target := HandName(guide[2])
 
-		// Determine if player won
-		winPoints := getWinPoints(choice, counter)
-		total += winPoints
-
-		// Determine player getHandScore
-		handPoints := getHandPoints(counter)
-		total += handPoints
-
-		log.Println("Result", total)
+		// Add opponent choice and player target to hands
+		hands = append(hands, Hand{oppChoice, target})
 	}
+
+	// Run day 1
+	resultDayOne(hands)
+
+	// Run day 2
+	resultDayTwo(hands)
 }
 
-func getWinPoints(choice string, counter string) int32 {
-	playerHand := getHandPoints(counter)
-	oppHand := getHandPoints(choice)
-
-	if playerHand == 1 && oppHand == 3 {
-		return 6
-	} else if playerHand == 2 && oppHand == 1 {
-		return 6
-	} else if playerHand == 3 && oppHand == 2 {
-		return 6
-	} else if playerHand == oppHand {
-		return 3
+func toHandName(value string) HandName {
+	if value == "A" || value == "X" {
+		return Rock
+	} else if value == "B" || value == "Y" {
+		return Paper
 	} else {
-		return 0
+		return Scissors
 	}
 }
 
-func getHandPoints(value string) int32 {
-	switch value {
-	case "A":
-		return 1
-	case "X":
-		return 1
-	case "B":
-		return 2
-	case "Y":
-		return 2
-	case "C":
-		return 3
-	case "Z":
-		return 3
+func resultDayOne(hands []Hand) {
+	log.Println("Result Day One")
+
+	var result int32
+
+	for _, hand := range hands {
+		// Get points from winning, losing, or drawing
+		oppHand := toHandName(string(hand.OppChoice))
+		targetHand := toHandName(string(hand.Target))
+		if gameMap[oppHand].DefeatedBy == targetHand {
+			result += WinPoints + gameMap[targetHand].Score
+		} else if gameMap[oppHand].Defeats == targetHand {
+			result += LosePoints + gameMap[targetHand].Score
+		} else {
+			result += DrawPoints + gameMap[targetHand].Score
+		}
 	}
-	return 0
+
+	log.Println("Day One Result", result)
 }
 
-func resultDayOne() {
-	log.Println("Result Day Onej")
-}
+func resultDayTwo(hands []Hand) {
+	var result int32
 
-func resultDayTwo() {
-	log.Println("Day Two")
+	for _, hand := range hands {
+		if hand.Target == "X" {
+			oppHand := toHandName(string(hand.OppChoice))
+			playerHand := gameMap[oppHand].Defeats
+			result += LosePoints + gameMap[playerHand].Score
+		} else if hand.Target == "Y" {
+			oppHand := toHandName(string(hand.OppChoice))
+			playerHand := oppHand
+			result += DrawPoints + gameMap[playerHand].Score
+		} else {
+			oppHand := toHandName(string(hand.OppChoice))
+			playerHand := gameMap[oppHand].DefeatedBy
+			result += WinPoints + gameMap[playerHand].Score
+		}
+	}
+
+	log.Println("Day Two Result", result)
 }
